06405aeb7aea87eb2ca3b0cb2a4bb856142b41fd
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-07-24 18:28:15 -0300

    Update dados-iniciais.sql
    novos dados no banco

--- ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,1 +1,6 @@
 insert into Produto (id,nome,preco) values (1,"Kindle", 600.0);
+insert into Produto (id, nome, preco) values (3, 'Câmera GoPro Hero 7', 1400.0);
+
+
+insert into Cliente (id,nome) values  (1,'Matheus');
+insert into Cliente (id,nome) values (2,'Fernanda');
