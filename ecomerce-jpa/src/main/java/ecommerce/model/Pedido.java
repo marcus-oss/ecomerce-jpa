62ca3c165efe405f47955147cdcc08c016e30684
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-07-29 17:37:52 -0300

    Pedido
    adicionando a classe Pedido com anotações do lombok

---
+++ ecomerce-jpa/src/main/java/ecommerce/model/Pedido.java
@@ -0,0 +1,39 @@
+package ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "pedido")
+public class Pedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Integer id;
+
+    @Column(name = "data_pedido")
+    private LocalDateTime dataPedido;
+
+    @Column(name = "data_conclusao_pedido")
+    private LocalDateTime dataConclusaoPedido;
+
+    @Column(name = "nota_fiscal_id")
+    private Integer notaFiscalId;
+
+    private BigDecimal Total;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    @Embedded
+    private EnderecoEntregaPedido entregaPedido;
+
+}
