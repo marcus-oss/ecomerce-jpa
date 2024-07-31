059567f4953fee8c92c56f81c87af0fd85bcb614
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-07-31 17:30:45 -0300

    Pedido.java
    adicionando o mtedo cliente com a anotação     @ManyToOne

--- ecomerce-jpa/src/main/java/ecommerce/model/Pedido.java
+++ ecomerce-jpa/src/main/java/ecommerce/model/Pedido.java
@@ -21,6 +21,10 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
     @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
 
