a053445a634c42f948208778a138c01e4d5a0961
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-08-01 22:18:12 -0300

    PagamentoCartao.java
    adiconando o metodo privado de pedidos com a anotoção do jpa @OneToOne

--- ecomerce-jpa/src/main/java/ecommerce/model/PagamentoCartao.java
+++ ecomerce-jpa/src/main/java/ecommerce/model/PagamentoCartao.java
@@ -24,8 +24,10 @@ public class PagamentoCartao {
     private Integer id;
 
 
-    @Column(name = "pedido_id")
-    private Integer pedidoId;
+  
+    @OneToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
 
     @Enumerated(EnumType.STRING)
     private StatusPagamento status;
