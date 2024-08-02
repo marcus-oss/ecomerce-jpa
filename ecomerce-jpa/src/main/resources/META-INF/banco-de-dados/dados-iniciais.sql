96086551cf498e50a1c54e8f3940e7d0687d9b8d
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-08-01 22:23:08 -0300

    dados-iniciais.sql
    adicionando novos inserts

--- ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -4,3 +4,9 @@ insert into Produto (id, nome, preco) values (3, 'Câmera GoPro Hero 7', 1400.0)
 
 insert into Cliente (id,nome) values  (1,'Matheus');
 insert into Cliente (id,nome) values (2,'Fernanda');
+
+insert into pedido ( id ,cliente_id,data_pedido, total, status) values (1, 1, sysdate(),  100.0, 'AGUARDANDO');
+
+insert into item_pedido (id, pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 1, 5.0, 2);
+
+insert  into categoria(id,nome) values (1,'Eletronicos');
