ef5365587bdd817e8c100911a7c5ce7d4e4d14c3
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-08-01 22:16:54 -0300

    Produto.java
    adiconando a lista de categorias com  a anotação do jpa @ManyToMany

--- ecomerce-jpa/src/main/java/ecommerce/model/Produto.java
+++ ecomerce-jpa/src/main/java/ecommerce/model/Produto.java
@@ -24,6 +24,9 @@ public class Produto {
 
     private BigDecimal preco;
 
+    @ManyToMany
+    @JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias;
 
 }
 
