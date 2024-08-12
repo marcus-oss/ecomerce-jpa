2f6f1f8996d867161b90b5c352ff4bc4481ba8c8
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-08-12 17:42:29 -0300

    Add files via upload
    adicionando a classe id do item pedido

---
+++ ecomerce-jpa/src/main/java/ecommerce/model/ItemPedidoId.java
@@ -0,0 +1,26 @@
+package ecommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class ItemPedidoId implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @Column(name = "pedido_id")
+    private Integer pedidoId;
+
+
+    @EqualsAndHashCode.Include
+    @Column(name = "produto_id")
+    private Integer produtoId;
+
+}
