c1e16cd6b0b760b7e17bbb55ad85089d15cb70da
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-05 18:01:53 -0300

    Add files via upload
    adicionando novos teste de heranças

---
+++ ecomerce-jpa/src/test/java/ecomerce/dados/test/mapeamentoAvancado/HerancaTest.java
@@ -0,0 +1,56 @@
+package ecomerce.dados.test.mapeamentoAvancado;
+
+import ecomerce.dados.test.EntityManagerTest;
+import ecommerce.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class HerancaTest extends EntityManagerTest {
+
+
+    @Test
+    public void salvarCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Fernanda Morais");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assertions.assertNotNull(clienteVerificacao.getId());
+    }
+
+    @Test
+    public void buscarPagamentos() {
+        List<Pagamento> pagamentos = entityManager
+                .createQuery("select p from Pagamento p")
+                .getResultList();
+
+        Assertions.assertFalse(pagamentos.isEmpty());
+    }
+
+
+    @Test
+    public void incluirPagamentoPedido() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setPedido(pedido);
+        pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
+        pagamentoCartao.setNumerocartao("123");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pagamentoCartao);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assertions.assertNotNull(pedidoVerificacao.getPagamento());
+    }
+}
