03d0e233c7ea7358a32e22c431b736996d094b36
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-09 17:23:08 -0300

    Add files via upload
    teste de ddl

---
+++ ecomerce-jpa/src/test/java/ecomerce/dados/test/DDLtest/DDLTest.java
@@ -0,0 +1,12 @@
+package ecomerce.dados.test.DDLtest;
+
+import ecomerce.dados.test.EntityManagerTest;
+import org.junit.jupiter.api.Test;
+
+public class DDLTest extends EntityManagerTest {
+
+    @Test
+    public void gerar_DDL() {
+        //teste vazio para apenas ver o ddl sendo gerado
+    }
+}
