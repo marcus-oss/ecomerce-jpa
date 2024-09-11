e8188c2ab077b375b4bf68b49157ee4dda80c772
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-10 22:04:42 -0300

    EnderecoEntregaPedido.java

--- ecomerce-jpa/src/main/java/ecommerce/model/EnderecoEntregaPedido.java
+++ ecomerce-jpa/src/main/java/ecommerce/model/EnderecoEntregaPedido.java
@@ -10,18 +10,24 @@ import lombok.Setter;
 public class EnderecoEntregaPedido {
 
 
+    @Column(length = 9)
     private String cep;
 
+    @Column(length = 100)
     private String logradouro;
 
+    @Column(length = 10)
     private String numero;
 
+    @Column(length = 50)
     private String complemento;
 
+    @Column(length = 50)
     private String bairro;
 
+    @Column(length = 50)
     private String cidade;
 
-
+    @Column(length = 2)
     private String estado;
 }
