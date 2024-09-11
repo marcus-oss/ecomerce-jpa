a60e28bad1b5145c5624ac56952369432e335000
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-10 22:09:06 -0300

    HerancaTest.java

--- ecomerce-jpa/src/test/java/ecomerce/dados/test/mapeamentoAvancado/HerancaTest.java
+++ ecomerce-jpa/src/test/java/ecomerce/dados/test/mapeamentoAvancado/HerancaTest.java
@@ -12,8 +12,10 @@ public class HerancaTest extends EntityManagerTest {
 
     @Test
     public void salvarCliente() {
-        Cliente cliente = new Cliente();
+      Cliente cliente = new Cliente();
         cliente.setNome("Fernanda Morais");
+        cliente.setSexo(SexoCliente.FEMININO);
+        cliente.setCpf("009");
 
         entityManager.getTransaction().begin();
         entityManager.persist(cliente);
