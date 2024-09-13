60dbb902defb48c42306b5857e40dfa6588d0924
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-13 17:43:07 -0300

    dados-iniciais.sql
    adicionando novos dados no banco na tabela categoria

--- ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -17,4 +17,5 @@ insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) value
 
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '123', null);
 
-insert  into categoria(id,nome) values (1,"Eletrodomesticos");
+insert  into categoria(id,nome) values (1,'Eletrodomesticos');
+insert into categoria (id, nome) values (2, 'Livros');
