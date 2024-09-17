50b91ebde2fbe3b901dbc53cd663115122d5bbb4
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-17 17:04:43 -0300

    classe dto
    classe do modelo dto para usar no teste com  jpql

---
+++ ecomerce-jpa/src/main/java/ecommerce/dto/ProdutoDTO.java
@@ -0,0 +1,18 @@
+package ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoDTO {
+
+    private Integer id;
+
+    private String nome;
+
+}
