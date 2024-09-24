13e35e590d44d14e09d043c36b8fd95e7ee7f99b
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-09-24 18:09:15 -0300

    dados-iniciais.sql
    novos dados no banco

--- ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -10,9 +10,16 @@ insert into cliente_detalhe (cliente_id, sexo, data_nascimento) values (2, 'MASC
 
 insert into pedido (id, cliente_id, data_pedido, total, status) values (1, 1, sysdate(), 998.0, "AGUARDANDO");
 insert into pedido (id, cliente_id, data_pedido, total, status) values (2, 1, sysdate(), 499.0, "AGUARDANDO");
+insert into pedido (id, cliente_id, data_criacao, total, status) values (2, 1, date_sub(sysdate(), interval 5 day), 499.0, 'AGUARDANDO');
+insert into pedido (id, cliente_id, data_criacao, total, status) values (3, 1, date_sub(sysdate(), interval 4 day), 3500.0, 'PAGO');
+insert into pedido (id, cliente_id, data_criacao, total, status) values (4, 2, date_sub(sysdate(), interval 2 day), 499.0, 'PAGO');
+
 
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 499, 2);
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (2, 1, 499, 1);
+insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (2, 1, 499, 1);
+insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (3, 4, 3500, 1);
+insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (4, 1, 499, 1);
 
 
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '123', null);
@@ -23,8 +30,13 @@ insert into categoria (nome) values ('Esportes');
 insert into categoria (nome) values ('Futebol');
 insert into categoria (nome) values ('Futsal');
 insert into categoria (nome) values ('Volei');
-
+insert into categoria (id, nome) values (6, 'Notebooks');
+insert into categoria (id, nome) values (7, 'Smartphones');
+insert into categoria (id, nome) values (8, 'Câmeras');
 
 insert  into produto_categoria(produto_id, categoria_id) values (1,2)
+insert into produto_categoria (produto_id, categoria_id) values (3, 8);
+insert into produto_categoria (produto_id, categoria_id) values (4, 8);
+
 
 insert into nota_fiscal (pedido_id, xml, data_emissao) values (2,'<xml />', sysdate());
