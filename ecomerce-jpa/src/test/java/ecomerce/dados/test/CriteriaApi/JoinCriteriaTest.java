c718d67124f36af84ab58afb21793ccf32602bee
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-03 21:40:47 -0300

    JoinCriteriaTest.java
    novos testes usando Join

--- ecomerce-jpa/src/test/java/ecomerce/dados/test/CriteriaApi/JoinCriteriaTest.java
+++ ecomerce-jpa/src/test/java/ecomerce/dados/test/CriteriaApi/JoinCriteriaTest.java
@@ -31,4 +31,81 @@ public class JoinCriteriaTest extends EntityManagerTest {
         Assertions.assertTrue(lista.size() == 5);
 
     }
+
+    
+    @Test
+    public void fazer_Join_On() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento", JoinType.LEFT);
+        joinPagamento.on(criteriaBuilder.equal(
+                joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 2);
+
+    }
+
+
+    @Test
+    public void fazer_Join_Left_Outer_Join() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento", JoinType.LEFT);
+
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertTrue(lista.size() == 2);
+
+    }
+
+    @Test
+    public void usar_Join_Fetch() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        root.fetch("notaFiscal", JoinType.LEFT);
+        root.fetch("pagamento", JoinType.LEFT);
+        root.fetch("cliente");
+
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        Pedido pedido = typedQuery.getSingleResult();
+        Assertions.assertNotNull(pedido);
+    }
+
+    @Test
+    public void Exercicio_buscar_Pedidos_Com_Produto_Especifico() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<ItemPedido, Produto> joinItemPedidoProduto = root
+                .join(Pedido_.itens)
+                .join("produto");
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(
+                joinItemPedidoProduto.get("id"), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
 }
