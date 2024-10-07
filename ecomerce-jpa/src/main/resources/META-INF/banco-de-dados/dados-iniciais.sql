6322438e63d2f375ccebeeb9d74af3c388baeb07
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-07 17:12:30 -0300

    dados-iniciais.sql
    novos dados no banco

--- ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -13,6 +13,7 @@ insert into pedido (id, cliente_id, data_pedido, total, status) values (2, 1, sy
 insert into pedido (id, cliente_id, data_criacao, total, status) values (2, 1, date_sub(sysdate(), interval 5 day), 499.0, 'AGUARDANDO');
 insert into pedido (id, cliente_id, data_criacao, total, status) values (3, 1, date_sub(sysdate(), interval 4 day), 3500.0, 'PAGO');
 insert into pedido (id, cliente_id, data_criacao, total, status) values (4, 2, date_sub(sysdate(), interval 2 day), 499.0, 'PAGO');
+insert into pedido (id, cliente_id, data_criacao, total, status) values (6, 2, sysdate(), 799.0, 'AGUARDANDO');
 
 
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 499, 2);
@@ -20,13 +21,13 @@ insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) value
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (2, 1, 499, 1);
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (3, 4, 3500, 1);
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (4, 1, 499, 1);
-
+insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (6, 1, 799, 1);
 
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '123', null);
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (3, 'RECEBIDO', 'cartao', '0123', null);
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (4, 'PROCESSANDO', 'cartao', '4567', null);
 insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (5, 'RECEBIDO', 'boleto', null, '8910');
-insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (6, 'PROCESSANDO', 'cartao', '1112', null);
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras,data_vencimento) values (6, 'PROCESSANDO', 'boleto', null,'45666',date_add(sysdate(),interval 2 day);
 
 
 insert  into categoria(nome) values ('Eletrodomesticos');
