ec7dfc170f247d42d0f7e304728b8e9fa410b032
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-09 17:13:06 -0300

    Sub Queries test
    classe de teste usando sub queries

---
+++ ecomerce-jpa/src/test/java/ecomerce/dados/test/CriteriaApi/SubQueriesTest.java
@@ -0,0 +1,67 @@
+package ecomerce.dados.test.CriteriaApi;
+
+import ecomerce.dados.test.EntityManagerTest;
+import ecommerce.model.Pedido;
+import ecommerce.model.Pedido_;
+import ecommerce.model.Produto;
+import ecommerce.model.Produto_;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SubQueriesTest extends EntityManagerTest {
+
+
+    @Test
+    public void pesquisar_Subqueries_Test_02() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<BigDecimal> subquery = criteriaQuery.subquery(BigDecimal.class);
+        Root<Pedido> subqueryRoot = subquery.from(Pedido.class);
+        subquery.select(criteriaBuilder.avg(subqueryRoot.get(Pedido_.TOTAL)).as(BigDecimal.class));
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(root.get(Pedido_.TOTAL), subquery));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(
+                "ID: " + obj.getId() + ", Total: " + obj.getTotal()));
+    }
+
+    @Test
+    public void pesquisar_Subqueries_Test() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<BigDecimal> subquery = criteriaQuery.subquery(BigDecimal.class);
+        Root<Produto> subqueryRoot = subquery.from(Produto.class);
+        subquery.select(criteriaBuilder.max(subqueryRoot.get(Produto_.preco)));
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get(Produto_.preco), subquery));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assertions.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(
+                "ID: " + obj.getId() + ", Nome: " + obj.getNome() + ", Preço: " + obj.getPreco()));
+    }
+}
