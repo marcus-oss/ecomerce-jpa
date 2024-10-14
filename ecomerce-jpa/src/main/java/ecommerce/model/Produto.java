2745e42a4fa0a58992cab598f364d990607c1557
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-14 17:47:57 -0300

    Produto.java

--- ecomerce-jpa/src/main/java/ecommerce/model/Produto.java
+++ ecomerce-jpa/src/main/java/ecommerce/model/Produto.java
@@ -10,14 +10,41 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+ @NamedNativeQueries({
+        @NamedNativeQuery(name = "produto_loja.listar",
+                query = "select id, nome, data_criacao, data_ultima_atualizacao, preco, foto from produto_loja",
+                resultClass = Produto.class),
+
+        @NamedNativeQuery(name = "ecm_produto.listar", query = "select * from ecm_produto",
+                resultSetMapping = "ecm_produto.Produto")
+})
+@SqlResultSetMappings({
+        @SqlResultSetMapping(name = "produto_loja.Produto",
+                entities = {@EntityResult(entityClass = Produto.class)}),
+        @SqlResultSetMapping(name = "ecm_produto.Produto",
+                entities = {@EntityResult(entityClass = Produto.class,
+                        fields = {
+                                @FieldResult(name = "id", column = "prd_id"),
+                                @FieldResult(name = "nome", column = "prd_nome"),
+                                @FieldResult(name = "preco", column = "prd_preco"),
+                                @FieldResult(name = "foto", column = "prd_foto"),
+                                @FieldResult(name = "dataCriacao", column = "prd_data_criacao"),
+                                @FieldResult(name = "dataUltimaAtualizacao",
+                                        column = "prd_data_ultima_atualizacao")
+                        })}),
+        @SqlResultSetMapping(name = "ecm_produto.ProdutoDTO", classes = {
+                @ConstructorResult(targetClass = ProdutoDTO.class, columns = {
+                        @ColumnResult(name = "prd_id", type = Integer.class),
+                        @ColumnResult(name = "prd_nome", type = String.class)
+                })
+
+
+        })
+})
+
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @EntityListeners({GenericoListener.class})
-@Entity
- @NamedQueries({@NamedQuery(name = "Produto.listar", query = "select p from Produto p"),
-
-        @NamedQuery(name = "Produto.listarPorCategoria", query = "select p from Produto p where " +
-                "exists (select 1 from Categoria c2 join c2.produtos p2 where p2 = p and c2.id = :categoria)")
-})       
+@Entity  
 @Table(name = "produto", uniqueConstraints = {@UniqueConstraint(name = "unq_nome", columnNames = {"nome"})},
         indexes = {@Index(name = "idx_nome", columnList = "nome")})
 public class Produto {
