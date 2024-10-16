6b4935b1769d92f3ef8b8a5224678a8d2b15f8b5
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-15 22:13:50 -0300

    script-remocao.sql
    novos script de remoção

--- ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/script-remocao.sql
+++ ecomerce-jpa/src/main/resources/META-INF/banco-de-dados/script-remocao.sql
@@ -2,3 +2,12 @@ drop table if exists testando;
 
 
 drop function if exists acima_media_faturamento;
+
+
+drop procedure if exists buscar_nome_produto;
+
+drop procedure if exists compraram_acima_media;
+
+drop procedure if exists ajustar_preco_produto;
+
+drop view if exists view_produto;
