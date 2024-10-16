810648f30b8527063917c1e963cf2c6928cf7307
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-15 22:07:54 -0300

    nova classe de DTO sobre categoria

---
+++ ecomerce-jpa/src/main/java/ecommerce/dto/CategoriaDTO.java
@@ -0,0 +1,19 @@
+package ecommerce.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoriaDTO {
+
+    private Integer id;
+
+    private String nome;
+
+}
