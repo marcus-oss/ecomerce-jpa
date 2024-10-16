da97513975a812c350ff08d2989a62474b9b3289
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-16 17:38:43 -0300

    nova classe de entidade

---
+++ ecomerce-jpa/src/main/java/ecommerce/model/EntidadeBaseInteger.java
@@ -0,0 +1,23 @@
+package ecommerce.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@MappedSuperclass
+public class EntidadeBaseInteger {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
