bed9b9ba38c2e02ef2b080cdb2779207a4e7e4fa
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-10-28 22:36:51 -0300

    EntityManagerFactoryTest
    nova classe EntityManager

---
+++ ecomerce-jpa/src/test/java/ecomerce/dados/test/EntityManagerFactoryTest.java
@@ -0,0 +1,35 @@
+package ecomerce.dados.test;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class EntityManagerFactoryTest {
+
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterAll
+    public static void tearDownAfterClass() {
+        entityManagerFactory.close();
+    }
+
+    public static void log(Object obj, Object... args) {
+        System.out.println(
+                String.format("[LOG " + System.currentTimeMillis() + "] " + obj, args)
+        );
+    }
+
+    public static void esperar(int segundos) {
+        try {
+            Thread.sleep(segundos * 1000);
+        } catch (InterruptedException e) {}
+    }
+}
